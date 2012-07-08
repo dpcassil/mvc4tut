15e5bd2b7b347b2c23e5c6304a46a36b84907753
Daniel Cassil <me@danielcassil.com>
2012-07-07 22:36:43 -0500

    MVC4Razor tutorial added to git

---
+++ MVC4Razor/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4Razor.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public ActionResult Welcome(string name, int numTimes = 1) {
+            ViewBag.Message = "Hellow " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
